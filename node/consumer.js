893cfda266c083b90e706d5e4b64dd1dd4b3a25d
timsu92 <33785401+timsu92@users.noreply.github.com>
2024-04-25 00:57:09 +0800

    fix(js): commit before exiting

--- node/consumer.js
+++ node/consumer.js
@@ -28,3 +28,30 @@ const consume = async () => {
 consume().catch((err) => {
   console.error("Error in consumer: " + err.message);
 });
+const errorTypes = ['unhandledRejection', 'uncaughtException']
+const signalTraps = ['SIGTERM', 'SIGINT', 'SIGUSR2']
+
+errorTypes.forEach(type => {
+  process.on(type, async e => {
+    try {
+      console.log(`process.on ${type}`)
+      console.error(e)
+      await consumer.disconnect();
+      await consumer.commitOffsets();
+      process.exit(0)
+    } catch (_) {
+      process.exit(1)
+    }
+  })
+})
+
+signalTraps.forEach(type => {
+  process.once(type, async () => {
+    try {
+      await consumer.disconnect()
+      await consumer.commitOffsets();
+    } finally {
+      process.kill(process.pid, type)
+    }
+  })
+})
