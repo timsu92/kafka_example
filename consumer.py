13a045b6a7fc3f478e0d8dae2b07a7fa024034c1
timsu92 <33785401+timsu92@users.noreply.github.com>
2024-04-24 23:30:12 +0800

    fix(python): read not-read message

--- consumer.py
+++ consumer.py
@@ -2,7 +2,11 @@ import datetime
 from kafka import KafkaConsumer
 from kafka.consumer.fetcher import ConsumerRecord
 
-consumer = KafkaConsumer('topic-test', bootstrap_servers='localhost:9092')
+consumer = KafkaConsumer(
+        'topic-test',
+        bootstrap_servers='localhost:9092',
+        auto_offset_reset="earliest"
+)
 print("已啟動。用SIGINT (CTRL+C) 中止程式")
 try:
     for msg in consumer:
