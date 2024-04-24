5fc1abad2f9480841d15bcfa2f18fcf7e28d9197
timsu92 <33785401+timsu92@users.noreply.github.com>
2024-04-24 23:49:38 +0800

    fix(python): commit progress before leaving

--- consumer.py
+++ consumer.py
@@ -5,7 +5,8 @@ from kafka.consumer.fetcher import ConsumerRecord
 consumer = KafkaConsumer(
         'topic-test',
         bootstrap_servers='localhost:9092',
-        auto_offset_reset="earliest"
+        auto_offset_reset="earliest",
+        group_id='test-group'
 )
 print("已啟動。用SIGINT (CTRL+C) 中止程式")
 try:
@@ -19,4 +20,5 @@ try:
         prefix = f"{msg.topic}[{msg.partition} | {msg.offset}] / {datetime.datetime.fromtimestamp(msg.timestamp / 1000)}"
         print(f"- {prefix} {msg.value.decode()}")
 except KeyboardInterrupt:
+    consumer.commit()
     consumer.close()
