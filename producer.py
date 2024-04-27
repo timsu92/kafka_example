c657bcb78f2136fb99d257273b31214ef9064cfe
timsu92 <33785401+timsu92@users.noreply.github.com>
2024-04-27 23:17:14 +0800

    fix(python): create topic before running producer

--- producer.py
+++ producer.py
@@ -3,12 +3,22 @@ import asyncio
 from random import randint
 from time import sleep
 from kafka import KafkaProducer
+import kafka
+from kafka.admin import NewTopic
 from kafka.producer.future import FutureRecordMetadata, RecordMetadata
+from kafka.protocol.admin import CreateTopicsResponse_v3
 
 producer = KafkaProducer(bootstrap_servers='localhost:9092')
 print("已啟動。用SIGINT (CTRL+C) 中止程式")
 
+topic = "topic-test"
+
 async def main():
+    admin = kafka.KafkaAdminClient(bootstrap_servers=['localhost:9092'])
+    topics = admin.list_topics()
+    if topic not in topics:
+        createResult: CreateTopicsResponse_v3 = admin.create_topics((NewTopic(topic, 2, 1),))  # type: ignore
+        print(f"topic {topic} created")
     id = randint(0, 1000)
     i = 0
     try:
